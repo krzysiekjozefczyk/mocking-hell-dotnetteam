75a2af940e725af38f2d2436ef271a206cf092f1
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-22 21:28:59 +0100

    Add illegal transfer validation

--- spec/transfer_spec.rb
+++ spec/transfer_spec.rb
@@ -4,14 +4,19 @@ RSpec.describe Transfer do
   context '.new' do
     subject(:account1) { Account.new 1, 'user1', 'pass1', 500.0 }
     subject(:account2) { Account.new 2, 'user2', 'pass2', 500.0 }
-    subject(:transfer) { Transfer.new 1, account1, account2, 100.0 }
+    subject(:transfer1) { Transfer.new 1, account1, account2, 100.0 }
+    subject(:transfer2) { Transfer.new 2, account1, account2, 1000.0 }
 
     it 'initializes new transfer' do
-      expect { transfer }.not_to raise_error
+      expect { transfer1 }.not_to raise_error
     end
 
     it 'validates legal transfer' do
-      expect(transfer.valid?).to be true
+      expect(transfer1.valid?).to be true
+    end
+
+    it 'validates illegal transfer' do
+      expect(transfer2.valid?).to be false
     end
   end
 end
