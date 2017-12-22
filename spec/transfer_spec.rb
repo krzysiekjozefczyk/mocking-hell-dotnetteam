b69ab2ded36fde8357e545452a205cb329425ee5
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-22 21:31:56 +0100

    Move transfer validations to separate context

--- spec/transfer_spec.rb
+++ spec/transfer_spec.rb
@@ -5,11 +5,17 @@ RSpec.describe Transfer do
     subject(:account1) { Account.new 1, 'user1', 'pass1', 500.0 }
     subject(:account2) { Account.new 2, 'user2', 'pass2', 500.0 }
     subject(:transfer1) { Transfer.new 1, account1, account2, 100.0 }
-    subject(:transfer2) { Transfer.new 2, account1, account2, 1000.0 }
 
     it 'initializes new transfer' do
       expect { transfer1 }.not_to raise_error
     end
+  end
+
+  context '.valid?' do
+    subject(:account1) { Account.new 1, 'user1', 'pass1', 500.0 }
+    subject(:account2) { Account.new 2, 'user2', 'pass2', 500.0 }
+    subject(:transfer1) { Transfer.new 1, account1, account2, 100.0 }
+    subject(:transfer2) { Transfer.new 2, account1, account2, 1000.0 }
 
     it 'validates legal transfer' do
       expect(transfer1.valid?).to be true
