d239572d9dabb598aa70ed09e9e35ca84f9c5ba3
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-12-22 21:23:53 +0100

    Change bool assertion

--- spec/transfer_spec.rb
+++ spec/transfer_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Transfer do
     end
 
     it 'validates legal transfer' do
-      expect transfer.valid?.to be_truthy
+      expect (transfer.valid?).to be true
     end
   end
 end
