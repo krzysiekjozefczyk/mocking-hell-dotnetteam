61d86743dd93a6793d333de1e937dac7105b95c3
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-01-06 19:16:53 +0100

    Add user count test

--- spec/user_manager_spec.rb
+++ spec/user_manager_spec.rb
@@ -12,4 +12,23 @@ RSpec.describe UserManager do
       end
     end
   end
+
+  context 'with some users' do
+    let(:users) {
+      [
+        (User.new 1, 'Jon', 'Son', '91090877666'),
+        (User.new 2, 'Anne', 'Smith', '92050455111'),
+        (User.new 3, 'Mark', 'Twain', '93040333222')
+      ]
+    }
+    subject(:manager) { UserManager.new users }
+
+    describe '.user_count' do
+      let(:expected_number) { 3 }
+
+      it 'returns correct number of users' do
+        expect(manager.user_count).to eq(expected_number)
+      end
+    end
+  end
 end
