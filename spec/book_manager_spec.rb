f00946df2079dc71ef82622905220c8f71a3641d
Patryk Miłek <patrykmilek96@gmail.com>
2018-01-06 17:28:57 +0100

    Add book count test

--- spec/book_manager_spec.rb
+++ spec/book_manager_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe BookManager do
       expect{ manager }.not_to raise_error
     end
   end
+
+  context 'with some books' do
+        let(:books) { [
+          (Book.new 1, 'John Doe', 'Red Book', 2000, 'science'),
+          (Book.new 2, 'John Doe', 'Green Book', 2001, 'science'),
+          (Book.new 3, 'John Doe', 'Blue Book', 2002, 'science')
+          ] }
+        subject(:manager) { BookManager.new books }
+        let(:expected_number) { 3 }
+
+        it 'returns correct number of books' do
+            expect(manager.get_book_count).to eq(expected_number)
+        end
+    end
 end
