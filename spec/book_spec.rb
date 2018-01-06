68d95421223e2a95bad4c40d40e3662b8f78ffd0
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-01-06 13:33:33 +0100

    Add book spec

---
+++ spec/book_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+RSpec.describe Book do
+  context '.new' do
+    subject(:book) { Book.new 1, 'Donald Knuth', 'The Art of Computer Programming', '1968', 'IT' }
+
+    it 'initializes a new book' do
+      expect { book }.not_to raise_error
+    end
+  end
+end
