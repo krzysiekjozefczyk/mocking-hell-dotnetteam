dc320655d1a4ef34985ed1c6240e196c82b58288
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-01-06 20:58:26 +0100

    Extend library with return book

--- lib/library.rb
+++ lib/library.rb
@@ -12,4 +12,12 @@ class Library
     user.overall_rented_books += 1
     user_manager.add_book(user.id, book)
   end
+
+  def return_book(user_id, book_id)
+    user = user_manager.get_user(user_id)
+    book = book_manager.get_book(book_id)
+
+    book.rented_by = nil
+    user_manager.remove_book(user.id, book)
+  end
 end
