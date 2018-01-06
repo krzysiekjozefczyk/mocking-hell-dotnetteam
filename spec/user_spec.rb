c9a3e7b472b346d29cb1a87234809bb0b69cca07
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-01-06 13:30:06 +0100

    Add user to_s test

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe User do
       expect { user }.not_to raise_error
     end
   end
+
+  context '.to_s' do
+    subject(:user) { User.new 1, 'John', 'Doe', '90010198765' }
+
+    it 'returns formatted first name and last name' do
+      expect(user.to_s).to be_a(String).and include('John Doe')
+    end
+  end
 end
