75047b95a0249d522b22e66ab0c1b547cb6913f3
Patryk Miłek <patrykmilek96@gmail.com>
2018-01-06 17:49:21 +0100

    Add book remove implementation

--- lib/book_manager.rb
+++ lib/book_manager.rb
@@ -1,19 +1,24 @@
 class BookManager
-    attr_accessor :books
+  attr_accessor :books
 
-    def initialize(books = [])
-        @books = books
-    end
+  def initialize(books = [])
+      @books = books
+  end
 
-    def get_book_count()
-        @books.count
-    end
+  def get_book_count()
+      @books.count
+  end
 
-    def get_book(id)
-      @books.find { |s| s.id = id}
-    end
+  def get_book(id)
+    @books.find { |s| s.id = id}
+  end
 
-    def add_book(book)
-        @books.push(book)
-    end
+  def add_book(book)
+      @books.push(book)
+  end
+
+  def remove_book(id)
+    book = get_book(id)
+    @books.delete(book)
+  end
 end
