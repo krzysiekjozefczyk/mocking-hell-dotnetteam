3150643dfc1f5992fa49110d980e544fdadc4357
Patryk Miłek <patrykmilek96@gmail.com>
2018-01-06 17:43:23 +0100

    Reorder context and method descriptions

--- spec/book_manager_spec.rb
+++ spec/book_manager_spec.rb
@@ -1,17 +1,10 @@
 require 'spec_helper'
 
 RSpec.describe BookManager do
-  describe '.new' do
+  context 'without books' do
     subject(:manager) { BookManager.new }
 
-    it 'initializes a new book manager' do
-      expect{ manager }.not_to raise_error
-    end
-  end
-
-  describe '.get_book_count' do
-    context 'without books' do
-      subject(:manager) { BookManager.new }
+    describe '.get_book_count' do
       let(:expected_number) { 0 }
 
       it 'returns 0' do
@@ -19,24 +12,7 @@ RSpec.describe BookManager do
       end
     end
 
-    context 'with some books' do
-      let(:books) { [
-        (Book.new 1, 'John Doe', 'Red Book', 2000, 'science'),
-        (Book.new 2, 'John Doe', 'Green Book', 2001, 'science'),
-        (Book.new 3, 'John Doe', 'Blue Book', 2002, 'science')
-      ] }
-      subject(:manager) { BookManager.new books }
-      let(:expected_number) { 3 }
-
-      it 'returns correct number of books' do
-        expect(manager.get_book_count).to eq(expected_number)
-      end
-    end
-  end
-
-  describe '.add_book' do
-    context 'without books' do
-      subject(:manager) { BookManager.new }
+    describe '.add_book' do
       let(:book) { Book.new 1, 'John Doe', 'Red Book', 2000, 'science' }
       let(:expected_number) { 1 }
 
@@ -48,4 +24,20 @@ RSpec.describe BookManager do
     end
   end
 
+  context 'with some books' do
+    let(:books) { [
+      (Book.new 1, 'John Doe', 'Red Book', 2000, 'science'),
+      (Book.new 2, 'John Doe', 'Green Book', 2001, 'science'),
+      (Book.new 3, 'John Doe', 'Blue Book', 2002, 'science')
+    ] }
+    subject(:manager) { BookManager.new books }
+
+    describe '.get_book_count' do
+      let(:expected_number) { 3 }
+
+      it 'returns correct number of books' do
+        expect(manager.get_book_count).to eq(expected_number)
+      end
+    end
+  end
 end
