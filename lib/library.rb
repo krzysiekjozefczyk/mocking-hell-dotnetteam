b3a28f989fe94c27270e55f889d2c9a14787d376
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-01-06 20:58:05 +0100

    Extend library with rent book

--- lib/library.rb
+++ lib/library.rb
@@ -3,4 +3,13 @@ class Library
     @user_manager = user_manager
     @book_manager = book_manager
   end
+
+  def rent_book(user_id, book_id)
+    user = user_manager.get_user(user_id)
+    book = book_manager.get_book(book_id)
+
+    book.rented_by = user.id
+    user.overall_rented_books += 1
+    user_manager.add_book(user.id, book)
+  end
 end
