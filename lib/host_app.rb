a6c26d1b8691ac54082ca948e674bbf65f2053c0
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2018-01-07 02:35:49 +0100

    Extend host app with user add

--- lib/host_app.rb
+++ lib/host_app.rb
@@ -20,10 +20,31 @@ class HostApp
   end
 
   def print_users
-    write '-- USERS: --'
+    write '-- USERS --'
     @user_manager.users.each { |x| write x.to_s }
+  end
+
+  def add_user
+    write '-- ADD NEW USER --'
+    user = create_user
+    result = @user_manager.add_user user
+    message = result.nil? ? 'ERROR' : 'OK'
+    write message
+  end
+
+  def create_user
+    write 'ID?'
+    id = read
+    write 'First name?'
+    first_name = read
+    write 'Last name?'
+    last_name = read
+    write 'PESEL?'
+    pesel = read
+    User.new id, first_name, last_name, pesel
   end
 end
 
 app = HostApp.new(ConsoleIo.new)
+app.add_user
 app.print_users
