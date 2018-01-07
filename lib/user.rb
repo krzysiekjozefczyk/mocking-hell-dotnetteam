6847ef20e6179ae6dd26eeb418d7bef1dedfed03
Patryk Miłek <patrykmilek96@gmail.com>
2018-01-07 14:00:19 +0100

    Extend user model with default books

--- lib/user.rb
+++ lib/user.rb
@@ -1,13 +1,13 @@
 class User
   attr_accessor :id, :first_name, :last_name, :pesel, :overall_rented_books, :currently_rented_books
 
-  def initialize(id, first_name, last_name, pesel)
+  def initialize(id, first_name, last_name, pesel, currently_rented_books = [])
     @id = id
     @first_name = first_name
     @last_name = last_name
     @pesel = pesel
     @overall_rented_books = 0
-    @currently_rented_books = []
+    @currently_rented_books = currently_rented_books
   end
 
   def to_s
